216d152b9e773ace8f8369be89cda0eaaaebba3f
lumi-tip <informacionlmdv@gmail.com>
2024-10-18 19:24:41 -0400

    attempt 4

--- webpack.dev.js
+++ webpack.dev.js
@@ -13,9 +13,11 @@ module.exports = merge(common, {
   //   publicPath: '/'
   // },
   devServer: {
-    contentBase:  './dist',
+    static: {
+      directory: path.join(__dirname, 'dist'), // Cambia 'contentBase' por 'static'
+    },
     hot: true,
-    disableHostCheck: true,
+    allowedHosts: 'all',
     historyApiFallback: true
   },
   plugins: [
